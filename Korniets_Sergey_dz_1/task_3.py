c20d19d21731a69f93b3c65729e987dcce4102fe
korado2022 <korado_29@mail.ru>
2022-03-02 18:30:05 +0300

    del

--- Korniets_Sergey_dz_1/task_3.py
+++
@@ -1,7 +0,0 @@
-for per in range(1, 101):
-    if 4 < per < 21 or 4 < per % 10 <= 9 or per % 10 == 0:
-        print(f'{per} процентов')
-    elif per == 1 or per % 10 == 1:
-        print(f'{per} процент')
-    elif 1 < per < 5 or 1 < per % 10 < 5:
-        print(f'{per} процента')
